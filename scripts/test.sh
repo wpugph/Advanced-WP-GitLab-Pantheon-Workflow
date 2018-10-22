67a88ab8d7eba7271ff197b0ff022b3072f72aa0
carl alberto <carl.alberto@pantheon.io>
2018-10-22 21:02:57 +0800

    reamoved dealer direct

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 
 set -ex
 ls
-phpcs -i
+#phpcs -i
 export THEMETOCHECK=$ROOTPWD/web/wp-content/themes/twentyseventeen-child
 export PHPCS_DIR=$ROOTPWD/vendor/squizlabs/php_codesniffer/bin #phpcs v >3
 export SNIFFS_DIR=$ROOTPWD/vendor/wp-coding-standards/wpcs
@@ -12,7 +12,7 @@ cd squizlabs
 cd php_codesniffer
 ls
 #cd $PHPCS_DIR/phpcs
-phpcs -i
-phpcs --config-set installed_paths $SNIFFS_DIR
+#phpcs -i
+#phpcs --config-set installed_paths $SNIFFS_DIR
 $PHPCS_DIR/phpcs --config-set installed_paths $SNIFFS_DIR
 $PHPCS_DIR/phpcs -p $THEMETOCHECK --standard=WordPress --ignore=$THEMEIGNORE
--- scripts/wp_build.sh
+++ scripts/wp_build.sh
@@ -10,9 +10,10 @@ pwd
 terminus auth:login --machine-token=$MACHINETOKEN --email=$EMAIL
 terminus connection:set $PANTHEONSITENAME.dev sftp
 
-rsync -Lvz --ipv4 -a --delete --progress -e 'ssh -p 2222' ./. --temp-dir=~/tmp/ $ENV.$SITE@appserver.$ENV.$SITE.drush.in:code/ --exclude=".git*" --exclude scripts/ vendor/
+rsync -Lvz --ipv4 -a --delete --progress -e 'ssh -p 2222' ./. --temp-dir=~/tmp/ $ENV.$SITE@appserver.$ENV.$SITE.drush.in:code/ --exclude .git* scripts/ vendor/
 
 rsync -Lvz --size-only --ipv4 -a --delete --progress -e 'ssh -p 2222' ./web/. --temp-dir=~/tmp/ $ENV.$SITE@appserver.$ENV.$SITE.drush.in:code/web/ --exclude=".git*"
+
 rsync -Lvz --size-only --ipv4 -a --delete --progress -e 'ssh -p 2222' ./vendor/. --temp-dir=~/tmp/ $ENV.$SITE@appserver.$ENV.$SITE.drush.in:code/vendor/ --exclude=".git*"
 
 rsync -rLvz --size-only --ipv4 --progress -e 'ssh -p 2222' ./web/. --temp-dir=~/tmp/ $ENV.$SITE@appserver.$ENV.$SITE.drush.in:code/web/ --exclude=".git*"
