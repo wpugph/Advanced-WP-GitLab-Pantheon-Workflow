a0f5e8d04fab582e8ae68d86568124324dc1ce39
carl alberto <carl.alberto@pantheon.io>
2018-10-22 20:51:46 +0800

    track phpcs

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,12 +2,16 @@
 
 set -ex
 ls
+phpcs -i
 export THEMETOCHECK=$ROOTPWD/web/wp-content/themes/twentyseventeen-child
 export PHPCS_DIR=$ROOTPWD/vendor/squizlabs/php_codesniffer/bin #phpcs v >3
 export SNIFFS_DIR=$ROOTPWD/vendor/wp-coding-standards/wpcs
 export THEMEIGNORE=*gulpfile.js*,*/assets/*,*.css*,*/gulp*.js
-cd $PHPCS_DIR/phpcs
+cd Vendor
+cd squizlabs
+cd php_codesniffer
 ls
+#cd $PHPCS_DIR/phpcs
 phpcs -i
 phpcs --config-set installed_paths $SNIFFS_DIR
 $PHPCS_DIR/phpcs --config-set installed_paths $SNIFFS_DIR
